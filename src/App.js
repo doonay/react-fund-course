237752197b78306cdb955fec821829ba446bd8b3
dementiado <dementiado@gmail.com>
2023-05-05 20:47:21 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './styles/App.css';
+import PostList from "./components/PostList";
+import PostForm from './components/PostForm';
+import PostFilter from './components/PostFilter';
+import MyModal from './components/UI/modal/MyModal';
+import MyButton from './components/UI/button/MyButton';
+import { usePosts } from './hooks/usePosts';
+import PostService from './API/PostService';
 
 function App() {
+  const [posts, setPosts] = useState([])
+  const [filter, setFilter] = useState({sort: '', query: ''})
+  const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+    setModal(false)
+  }
+
+  async function fetchPosts() {
+    const posts = await PostService.getAll();
+    setPosts(posts)
+    console.log(posts)
+  }
+
+
+
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchPosts}>GET POSTS</button>
+      <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+        Создать пользователя
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost}/>
+      </MyModal>
+      <hr style={{margin: '15px 0'}}/>
+      <PostFilter
+        filter={filter}
+        setFilter={setFilter}
+      />
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Все игры"/>
     </div>
   );
 }
---
+++ src/components/PostList.jsx
@@ -0,0 +1,39 @@
+import React, {nodeRef} from 'react';
+import PostItem from "./PostItem";
+import {CSSTransition, TransitionGroup,} from 'react-transition-group';
+
+
+
+
+const PostList = ({posts, title, remove}) => {
+
+  if (!posts.length) {
+    return (
+      <h1 style={{textAlign: 'center'}}>
+        Игры не найдены!
+      </h1>
+    )
+  }
+
+  return (
+    <div>
+        <h1 style={{textAlign: 'center'}}>
+        {title}
+        </h1>
+        <TransitionGroup>
+          {posts.map((post, index)=>
+            <CSSTransition
+              key={post.id}
+              nodeRef={nodeRef}
+              timeout={500}
+              classNames="post"
+            >
+            <PostItem remove={remove} number={index + 1} post={post}/>
+            </CSSTransition>
+          )}
+        </TransitionGroup>
+    </div>
+  );
+};
+
+export default PostList;
